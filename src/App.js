d568c8e3e04685ef9a0b8259266845cd3dab064b
Adaane <adaane.ahamada@hetic.net>
2020-09-23 13:44:44 +0200

    add onPlayerChoice func

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react'
 import './App.css';
 import Player from './components/Player';
 
 
-// const Game = (choiceOfPlayerOne, choiceOfPlayerTwo ) => {
 
-//   if choiceOfPlayerOne === 
-//   return choiceOfPlayerOne;
-// }
 
 function App() {
+
+
+  const [players, handlePlayers] = useState(
+    [
+      {
+        name: 'Adaane',
+        choice: ''
+      },
+      {
+        name: 'recruteur',
+        choice: ''
+      }
+    ]
+  );
+
+
+  const onPlayerChoice = (player, choice) => {
+
+    // une nouvelle variale pour stocker le nouvelle état des joueurs
+    // avec Reduce pour itérer le tableau des joueurs actuelles et y ajouter leurs choix
+    const newPlayers = players.reduce((acc, curr) => {
+      //si le joueur courant correspond au joueur sur lequelle j'ai cliqué
+      if (player === curr.name ) {
+        // Ajoute lui le choix que j'ai fais en cliquant
+        curr.choice = choice
+        // puis return l'accumulateur avec les nouveaux élément
+      }
+      return acc = [...acc, curr]
+
+    }, [])
+    //  On change le state de l'application avec le choix des joueurs 
+    handlePlayers(newPlayers)
+  };
+
+  // const ChooseWinner = (players) => {
+
+  // }
+
   return (
     <div className="App" style={{'display': 'flex'}}>
-      <Player score={0}/>
-      <Player score={0}/>
+      {
+        players.map((player, index) => {
+          return <Player score={0} 
+                    onPlayerChoice={onPlayerChoice} 
+                    name={player.name}
+                    choice={player.choice}
+                    key={index}/>
+        })
+      }
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -2,20 +2,36 @@ import React, {useState} from 'react'
 
 export default function Player(props) {
     // Le choix du Jouer
-    const [choice, setChoice] = useState('');
+    // const [choice, setChoice] = useState('');
+
+    const items = [{
+        name: 'Pierre',
+        id: 'R'
+    },
+    {
+        name: 'Papier',
+        id: 'P'
+    },
+    {
+        name: 'Ciseaux',
+        id: 'S'
+    }
+]
 
     return (
         <div>
-            <h1>Player One</h1>
+            <h1>Player  : {props.name}</h1>
             <h2>Choisis en un</h2>
 
             <ul>
-                <li onClick={() => setChoice('rock')}>Pierre</li>
-                <li onClick={() => setChoice('paper')}> Papier</li>
-                <li onClick={() => setChoice('scissors')}>Ciseaux</li>
+            {items.map( (item, index) => {
+                return <li key={index+1} onClick={() => {
+                    props.onPlayerChoice(props.name, item)
+                }}> {item.name} </li>
+            })}
             </ul>
 
-            <p>Vous avez Choisis {choice}</p>
+            <p>Vous avez Choisis {props.choice.name}</p>
             <p>Vous score est de  {props.score}</p>
         </div>
     )
